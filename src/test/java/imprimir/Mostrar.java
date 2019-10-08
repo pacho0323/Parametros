069caaed640d5d688348c082d12d6b1139e3331d
Juan Francisco Builes Montoya <pacho0323@hotmail.com>
2019-10-07 21:10:43 -0500

    subir datos

---
+++ src/test/java/imprimir/Mostrar.java
@@ -0,0 +1,15 @@
+package imprimir;
+
+
+import org.junit.Test; 
+
+public class Mostrar {
+	
+@Test
+	public void Mostrar() {	
+	String greeting = System.getenv("dato");
+		System.out.println(greeting);
+
+	}
+
+}
